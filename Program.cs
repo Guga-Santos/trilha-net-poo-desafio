36a2f93bf775b87f43f523cf4efb6d51f81b93a3
Guga <guga@Homme.Guga>
2024-01-03 12:13:53 -0300

    feat: testes no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("99999999", "6120", "123123", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("98888888", "12", "123321", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
